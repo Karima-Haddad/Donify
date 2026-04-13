cd21997a37685e1fb66f0f3abecaaedba709768b
Mayssa Chikh Ali <mayssa.cheikha.ali@gmail.com>
2026-04-13 22:25:52 +0100

    update profil donor + hospital avec toast personnalisé

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,10 +15,15 @@ import HospitalProfileEdit from "./pages/HospitalProfileEdit";
 import HospitalProfil from "./pages/HospitalProfil";
 import BloodRequestHistory from "./pages/BloodRequestHistory";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
     <>
+      {/* 🔔 Toast container doit être ICI (hors Routes) */}
+      <ToastContainer position="top-right" autoClose={3000} />
+
       <Routes>
         {/* Public */}
         <Route path="/" element={<Login />} />
